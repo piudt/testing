3fd25d6f7bf7f1cacae3ebccac9ded2892c16420
Tiara Ratna Sari <tiaratnasari12@gmail.com>
2015-04-02 13:49:35 +0700

    Tabel Model Pembayaran
    Tiara Ratna Sari

--- IClinic_ClientApp/src/ClientApplication/model/TableModelPembayaran.java
+++ IClinic_ClientApp/src/ClientApplication/model/TableModelPembayaran.java
@@ -12,7 +12,7 @@ import javax.swing.table.AbstractTableModel;
 
 /**
  *
- * @author Tiara Ratna Sari
+ * @author Tiara Ratna Sari1
  */
 public class TableModelPembayaran extends AbstractTableModel {
 
